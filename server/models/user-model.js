a0f76d56e7ea976c43f86589e968a14abd9e1599
unknown <mrhassenfadhlaoui@gmail.com>
2018-07-18 18:48:28 +0100

    First model is working

--- server/models/user-model.js
+++ server/models/user-model.js
@@ -1,23 +1,33 @@
-
-    var user =  {
-        '1' : {
-            "name" : "user1",
-            "password" : "pwd1",
-            "age" : "12"
-        },
-        '2' : {
-            "name" : "user2",
-            "password" : "pwd2",
-            "age" : "22"
-        },
-        '3' : {
-            "name" : "user3",
-            "password" : "pwd3",
-            "age" : "32"
-        }
-      };
+const db = require ('../database/connection.js');
 
 
+// USer model
+  const User = db.sequelize.define('user', {
+    firstName: {
+      type: db.Sequelize.STRING
+    },
+    lastName: {
+      type: db.Sequelize.STRING
+    }
+  });
 
 
-module.exports =  user  ;
+  // force: true will drop the table if it already exists
+  db.sequelize.sync({
+    logging : console.log}
+).then(() => {
+    // Table created
+    var userInstance =  User.build({
+      firstName: 'First user',
+      lastName: 'Hancock'
+    })
+
+    userInstance.save()
+
+  });
+
+
+
+
+
+ module.exports =  User  ;
